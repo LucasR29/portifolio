9370a19ddc04728a1ec8b7309bb140391566a05f
LucasR <lrigon2904@gmail.com>
2023-04-14 09:48:54 -0300

    fix: whats

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -2,7 +2,7 @@ export const userData = {
   nameUser: "Lucas Rigon",
   githubUser: "LucasR29",
   linkedinUser: "lucas-rigon",
-  whatsappNumber: "+5541988819028",
+  whatsappNumber: "+5541998630092",
   emailUser: "lrigon29042@gmail.com",
   instagramUser: "",
   facebookUser: "",
